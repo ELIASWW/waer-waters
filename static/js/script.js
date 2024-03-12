73d1489cdd5b486bea42db94f1e203649f93c9db
Witzevds <witzvand@student.arteveldehs.be>
2024-03-12 21:55:49 +0100

    vertaling in javascript

--- static/js/script.js
+++ static/js/script.js
@@ -7,36 +7,24 @@ const overlay$ = document.querySelector(".overlay");
 const body = document.querySelector("body");
 
 const letterWWallpapers = [
-  "iPhone 14 & 15 Pro - 1.png",
-  "iPhone 14 & 15 Pro - 2.png",
-  "iPhone 14 & 15 Pro - 3.png",
-  "iPhone 14 & 15 Pro - 13.png",
-  "iPhone 14 & 15 Pro - 14.png",
-  "iPhone 14 & 15 Pro - 15.png",
+  "iPhone 14 & 15 Pro - 1",
+  "iPhone 14 & 15 Pro - 2",
+  "iPhone 14 & 15 Pro - 3",
 ];
 const letterAWallpapers = [
-  "iPhone 14 & 15 Pro - 7.png",
-  "iPhone 14 & 15 Pro - 8.png",
-  "iPhone 14 & 15 Pro - 9.png",
-  "iPhone 14 & 15 Pro - 19.png",
-  "iPhone 14 & 15 Pro - 20.png",
-  "iPhone 14 & 15 Pro - 21.png",
+  "iPhone 14 & 15 Pro - 7",
+  "iPhone 14 & 15 Pro - 8",
+  "iPhone 14 & 15 Pro - 9",
 ];
 const letterCWallpapers = [
-  "iPhone 14 & 15 Pro - 4.png",
-  "iPhone 14 & 15 Pro - 5.png",
-  "iPhone 14 & 15 Pro - 6.png",
-  "iPhone 14 & 15 Pro - 22.png",
-  "iPhone 14 & 15 Pro - 23.png",
-  "iPhone 14 & 15 Pro - 24.png",
+  "iPhone 14 & 15 Pro - 4",
+  "iPhone 14 & 15 Pro - 5",
+  "iPhone 14 & 15 Pro - 6",
 ];
 const letterOWallpapers = [
-  "iPhone 14 & 15 Pro - 10.png",
-  "iPhone 14 & 15 Pro - 11.png",
-  "iPhone 14 & 15 Pro - 12.png",
-  "iPhone 14 & 15 Pro - 16.png",
-  "iPhone 14 & 15 Pro - 17.png",
-  "iPhone 14 & 15 Pro - 18.png",
+  "iPhone 14 & 15 Pro - 10",
+  "iPhone 14 & 15 Pro - 11",
+  "iPhone 14 & 15 Pro - 12",
 ];
 
 letterW$.addEventListener("click", () => {
@@ -44,28 +32,51 @@ letterW$.addEventListener("click", () => {
   let randomWallpaper = letterWWallpapers[randomIndex];
   images$.style.display = "none";
   overlay$.style.display = "block";
-  overlay$.innerHTML = `<img src="static/img/letter-W/${randomWallpaper}" alt="Letter W" />`;
+  overlay$.innerHTML = `<img src="static/img/letter-W/${randomWallpaper}.png" alt="Letter W" />
+    <button class="french">Français</button>`;
+  const button$ = document.querySelector(".french");
+  button$.addEventListener("click", () => {
+    overlay$.innerHTML = `<img src="static/img/letter-W/${randomWallpaper}FR.png" alt="Letter W" />`;
+    console.log("français");
+  });
 });
 letterA$.addEventListener("click", () => {
   let randomIndex = Math.floor(Math.random() * letterAWallpapers.length);
   let randomWallpaper = letterAWallpapers[randomIndex];
   images$.style.display = "none";
   overlay$.style.display = "block";
-  overlay$.innerHTML = `<img src="static/img/letter-A/${randomWallpaper}" alt="Letter A" />`;
+  overlay$.innerHTML = `<img src="static/img/letter-A/${randomWallpaper}.png" alt="Letter A" />
+    <button class="french">Français</button>`;
+  const button$ = document.querySelector(".french");
+  button$.addEventListener("click", () => {
+    overlay$.innerHTML = `<img src="static/img/letter-A/${randomWallpaper}FR.png" alt="Letter A" />`;
+    console.log("français");
+  });
 });
-
 letterC$.addEventListener("click", () => {
   let randomIndex = Math.floor(Math.random() * letterCWallpapers.length);
   let randomWallpaper = letterCWallpapers[randomIndex];
   images$.style.display = "none";
-  overlay$.classList.add("show-overlay");
-  overlay$.innerHTML = `<img src="static/img/letter-C/${randomWallpaper}" alt="Letter C" />`;
+  overlay$.style.display = "block";
+  overlay$.innerHTML = `<img src="static/img/letter-C/${randomWallpaper}.png" alt="Letter C" />
+    <button class="french">Français</button>`;
+  const button$ = document.querySelector(".french");
+  button$.addEventListener("click", () => {
+    overlay$.innerHTML = `<img src="static/img/letter-C/${randomWallpaper}FR.png" alt="Letter C" />`;
+    console.log("français");
+  });
 });
-
 letterO$.addEventListener("click", () => {
   let randomIndex = Math.floor(Math.random() * letterOWallpapers.length);
   let randomWallpaper = letterOWallpapers[randomIndex];
   images$.style.display = "none";
   overlay$.style.display = "block";
-  overlay$.innerHTML = `<img src="static/img/letter-O/${randomWallpaper}" alt="Letter O" />`;
+  overlay$.innerHTML = `<img src="static/img/letter-O/${randomWallpaper}.png" alt="Letter O" />
+    <button class="french">Français</button>`;
+
+  const button$ = document.querySelector(".french");
+  button$.addEventListener("click", () => {
+    overlay$.innerHTML = `<img src="static/img/letter-O/${randomWallpaper}FR.png" alt="Letter W" />`;
+    console.log("français");
+  });
 });
